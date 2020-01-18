b922a54fba08da49c5cc7db679d9af5f344c431b
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-01-17 17:35:26 -0700

    working on categories and cards layout

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="w-20 vh-25 flex bg-white">
+            Card
+        </div>
+    )
+}
+
+export default Card;
